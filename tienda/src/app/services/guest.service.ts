3e4438feef0b627f28f7e1f997fb16e57549764a
Dh4ki <edu_gv_14@hotmail.com>
2022-11-27 01:06:19 -0500

    Feat: buscando ubigeo
    Se tuvo que importar un archivo JSON donde tenia la data de los paises,regiones,provincias y dsitritos,
    Se solucionó el problema de desactivar y activar pasies o distritos.

--- tienda/src/app/services/guest.service.ts
+++ tienda/src/app/services/guest.service.ts
@@ -27,4 +27,15 @@ export class GuestService {
     return this._http.get(this.url+'listar_productos_recomendado_publico/'+categoria,{headers:headers});
   }
 
+  get_regiones():Observable<any>{
+    return this._http.get('./assets/regiones.json');
+  }
+
+  get_distritos():Observable<any>{
+    return this._http.get('./assets/distritos.json');
+  }
+  get_provincias():Observable<any>{
+    return this._http.get('./assets/provincias.json');
+  }
+
 }
